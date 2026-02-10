94179e24087a950de814568cdb5b6a8f92f1a31f
Liqour-ice <256075236+edenroxannebiong-lgtm@users.noreply.github.com>
2026-02-10 19:20:33 +0800

    fix build issue

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -11,12 +11,10 @@ const transitionVariants = {
     item: {
         hidden: {
             opacity: 0,
-            filter: 'blur(12px)',
             y: 12,
         },
         visible: {
             opacity: 1,
-            filter: 'blur(0px)',
             y: 0,
             transition: {
                 type: 'spring',
